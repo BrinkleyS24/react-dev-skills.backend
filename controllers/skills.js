4f5f58ffdbb51837c87109c757bf158ac997061f
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-01-24 16:32:13 -0500

    controllers update

--- controllers/skills.js
+++ controllers/skills.js
@@ -5,17 +5,16 @@ const skillsRouter = express.Router();
 //Index
 skillsRouter.get('/', (req, res) => {
     Skill.find({}, (error, allSkills) => {
-        res.json({
-            response: "Hello World",
-            skills: allSkills
-        })
+        res.json(
+         allSkills
+        )
     })
 })
 
 //Create
 skillsRouter.post("/", (req, res) => {
     Skill.create(req.body, (error, createdSkill) => {
-        res.json()
+        res.json(createdSkill)
     })
 })
 
